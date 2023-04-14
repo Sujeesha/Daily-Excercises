17ae6629bd5a85f628607b01fc1d054165a8afaf
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-04-14 17:49:37 +0530

    Add files via upload

---
+++ day 10/IndexerExample/IndexerExample/Program.cs
@@ -0,0 +1,21 @@
+﻿//Example for Indexer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            array a;
+            a = new array();
+            a[3] = 43.2f;
+            Console.WriteLine(a[3]);
+
+        }
+    }
+}
---
+++ day 10/ListExamples/ListExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿//Examples of List
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add prime elements to List and print it
+            List<int> PrimeNumbers = new List<int>();
+            PrimeNumbers.Add(1);    
+            PrimeNumbers.Add(3);
+            PrimeNumbers.Add(5);
+            PrimeNumbers.Add(7); 
+            foreach (int i in PrimeNumbers)
+            {
+                Console.WriteLine(i);   
+            }
+
+
+        }
+    }
+}
