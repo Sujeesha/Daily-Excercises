1b951ad03fa3e240d6b3f117a99ce333cdd4bd6c
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-04-25 15:17:26 +0530

    Add files via upload

---
+++ day 12/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    internal class Program
+    {
+        static void Add(int i, int j)
+        {
+            Console.WriteLine("Sum: " + (i + j));
+        }
+        static void Sub(int i, int j)
+        {
+            Console.WriteLine("Sub: " + (i -j));
+        }
+        public delegate void reftomethod(int i, int j);
+        static void Main(string[] args)
+        {
+            reftomethod multicast = Add;
+            multicast += Sub;
+            multicast(20, 10);
+
+        }
+    }
+}
---
+++ day 12/ExceptionDemo/ExceptionDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿//Exception handling demo
+using System;
+
+
+namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 10, i = 0;
+            int[] a = new int[5];
+            try
+            {
+                k = k / i;               //divide by zero error
+                a[12] = 9;               //index out of bounds
+                
+            }
+            catch (DivideByZeroException e) {
+                System.Console.WriteLine("Divide by zero");
+
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                System.Console.WriteLine("Index out of bounds");
+            }
+            finally
+            {
+                System.Console.WriteLine("finally");
+            }
+            Console.WriteLine("Remaining program");
+
+        }
+    }
+}
