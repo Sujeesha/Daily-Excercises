33538edc08681d457486948ad191ae73124c27f2
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-05-08 09:35:55 +0530

    Add files via upload

---
+++ day 24/Entframe/Entframe/Program.cs
@@ -0,0 +1,23 @@
+﻿using Entframe.Entf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entframe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SchoolContext ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
