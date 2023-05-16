f3c425a186456e4340e91b1413d9a6a61efc5c79
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-05-16 10:12:49 +0530

    Add files via upload

---
+++ day 25/Entframe/Entframe/Entf/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entframe.Entf
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
