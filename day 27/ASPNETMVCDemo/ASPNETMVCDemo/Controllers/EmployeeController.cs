a345e070c74788fd7f791357d0f845542be5600e
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-05-23 09:24:58 +0530

    Add files via upload

---
+++ day 27/ASPNETMVCDemo/ASPNETMVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string Display()
+        {
+            return "Hello MVC ";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ day 27/ASPNETMVCDemo/ASPNETMVCDemo/Models/CustomerMaster.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPNETMVCDemo.Models
+{
+    public class CustomerMaster
+    {
+        public int CustId { get; set; } 
+        public string? Name { get; set; } 
+        public decimal Balance { get; set; }
+
+    }
+}
