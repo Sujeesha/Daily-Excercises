15c48ac17ecf0a18db6a887e04d44c840d395ccd
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-05-23 09:20:28 +0530

    Add files via upload

---
+++ day 26/Duplicate/Duplicate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+            int[] a = arr.Distinct();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+
+
+        }
+    }
+}
+
