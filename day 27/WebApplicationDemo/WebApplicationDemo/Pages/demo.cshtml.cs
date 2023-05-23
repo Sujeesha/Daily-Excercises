1dcafdb3188d17eeb4f9d02cf7ab2567fe8275e1
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-05-23 09:40:22 +0530

    Add files via upload

---
+++ day 27/WebApplicationDemo/WebApplicationDemo/Pages/demo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationDemo.Pages
+{
+    public class demoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
